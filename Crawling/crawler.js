a29c5db68bb922f9e0156af27b9cbe351fc4b4dd
Dmitri <dmitri.rg@gmail.com>
2019-11-22 12:15:34 -0600

    Commented Variables and functions

--- Crawling/crawler.js
+++ Crawling/crawler.js
@@ -29,37 +29,42 @@ start_crawling();
 
 async function start_crawling(){
 
-    const $rootPage = await rawPage.toJQsearchable(start_url);
-    let ListOfURLsOnRootPage = await rootPage.extractAllURLs($rootPage, site);
+    //creates an array of tuples in the form (label, URL),
+    //Where label is the text to it's respecive URL
+    //const $rootPage = await rawPage.toJQsearchable(start_url);
+    let ListOfURLsOnRootPage = await rootPage.extractAllURLs(await rawPage.toJQsearchable(start_url), site);
     console.log('Got all urls from first page. Moving On...')
 
-
+    //finds the tuple in ListOfURLsOnRootPage that contains our taget string in first element of tuple
     const booksindex = forcedPath.findURLforMatchString(ListOfURLsOnRootPage, target);
 
-    let urlto2 = ListOfURLsOnRootPage[booksindex][1];
-    console.log(urlto2);
+    //grabs the URL from target tuple 
+    let chosenSubcatURL = ListOfURLsOnRootPage[booksindex][1];
 
-    const $chtmlp2 = await rawPage.toJQsearchable(urlto2);
-
-    let p2 = await bookSubCatChoicePage.getSubCatUrls($chtmlp2);
+    //Provides a list of subcatgory URLs. Specifically runs on amazons Primary book page
+    //The bookSubCatChoicePage object runs getSubCatUrls function that takes in a cheero object that is returned from the rawPage function
+    let ListOfSubcatURLS = await bookSubCatChoicePage.getSubCatUrls(await rawPage.toJQsearchable(chosenSubcatURL));
     console.log('Got subcategorgie URLs on second page. Moving on...')
-    let urlto3 = 'https://www.amazon.com' + p2[1];
+    let productsListPage = 'https://www.amazon.com' + ListOfSubcatURLS[1];
     
-    const $ = await rawPage.toJQsearchable(urlto3);
     
-    //$$ = cheerio.load($('#mainResults').html());
-    let p3 = await results.extractProductLinks($);
-    //let p3 = await final_page_with_results(urlto3);
+    //Provides a list of subcatgory URLs. This should work on on final search reult pages as long as the primary search results has the div ID ##mainResults
+    //The results object runs extractProductLinks function that takes in a cheero object that is returned from the rawPage function
+    let ListOfProductURLs = await results.extractProductLinks(await rawPage.toJQsearchable(productsListPage));
     console.log('Got final page with products. Moving on...')
-    //console.log(p3);
+ 
 
-     for (var i = 0; i < p3.length; i++) {
-         const prodURL = p3[i];
+    /**Creates an array.
+     * Each element in the array contains a product and product details object
+     * Each object is formated in a way that we can convert to json latet
+     */
+     for (var i = 0; i < ListOfProductURLs.length; i++) {
+         const prodURL = ListOfProductURLs[i];
          const $ = await rawPage.toJQsearchable(prodURL);
          products.push(await product.extractProductDetails($, i, prodURL));
      }
 
-    //toJSONfile(products);
+ 
     jsonElementsArray.convertToJSON(products);
     console.log('We\'re done here. Check productjson.txt that was generated');
 }
